36054389b5fd385d806a2a7fd867a97bcd2eb985
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-13 14:12:30 +1200

    fixed input type and placeholder in cart component

--- rs-app/src/components/cart/cart.js
+++ rs-app/src/components/cart/cart.js
@@ -12,7 +12,7 @@ const Cart = () => {
                 <div className="cart-credit-container">
                     <h3 className="credit-title">Your Credit</h3>
                     <div className="current-credit">Current Credit: </div>
-                    <input className="credit-to-add">Enter amount</input>
+                    <input type='number' className="credit-to-add" placeholder="Enter amount"/>
                     <button className="add-credit">Add Credit</button>
                 </div>
                 <div className="order-summary-container">
@@ -20,7 +20,7 @@ const Cart = () => {
                     <div>menu items</div>
                     <div className="order-total">Order Total (Subtotal): </div>
                     <div className="tip-title">Tip</div>
-                    <input className="tip-to-add">Enter tip amount</input>
+                    <input className="tip-to-add" type='number' placeholder="Enter tip amount"/>
                     <button className="add-tip">Add Tip</button>
                     <div className="tip-amount">Tip Amount: </div>
                     <div className="grand-total">Grand Total: </div>
@@ -31,4 +31,6 @@ const Cart = () => {
 
 
     );
-}
+};
+
+export default Cart;
