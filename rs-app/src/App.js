99b89e8f10272bfb6dd05f656ae8050ccbc69723
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-13 15:18:04 +1200

    added toggle function on cart image

--- rs-app/src/App.js
+++ rs-app/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import AppHeader from './components/appHeader/appHeader';
 import Menu from './components/menu/menu';
-import Cart from './components/cart/cart';
+
 
 function App() {
   return (
     <div className="App">
       <AppHeader/>
       <Menu/>
-      <Cart/>
+     
     </div>
   );
 }
--- rs-app/src/components/appHeader/appHeader.js
+++ rs-app/src/components/appHeader/appHeader.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import cartIcon from './cart_icon.png';
 import rsLogo from './logo_black.png';
+import Cart from '../cart/cart';
 import './appHeader.css';
 
 
 const AppHeader = () => {
 
+    const [isCartVisible, setCartVisible] = useState(false);
 
+    const toggleCart = () => {
+        setCartVisible(!isCartVisible);
+    };
 
 
 
@@ -15,12 +20,15 @@ const AppHeader = () => {
         <header className='header-page'>
             <div className='header-top'>
                 <img className='rs-logo' src={rsLogo} alt='cloche with RS written on top'/>
+                <button className='cart-button' onClick={toggleCart}>
                 <img className='cart-icon' src={cartIcon} alt='shopping cart icon'/>
+                    </button>
             </div>
             <div className='header-container'>
                 <div className='title'>Restaurant Simulator</div>
                 <div className='sub-title'>Easy Ordering, Yummy Meals</div>
             </div>
+            {isCartVisible && <Cart />}
         </header>
     );
 };
