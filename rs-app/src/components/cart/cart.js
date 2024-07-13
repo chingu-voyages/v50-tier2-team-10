32c5e45c4f98e965ac55140fd81601b462f6c7e5
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-13 14:06:09 +1200

    created cart component and placeholders

---
+++ rs-app/src/components/cart/cart.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Cart = () => {
+
+
+
+
+    return (
+        <div>
+            <div className="cart-contents">
+                <div className="cart-title">Your Cart</div>
+                <div className="cart-credit-container">
+                    <h3 className="credit-title">Your Credit</h3>
+                    <div className="current-credit">Current Credit: </div>
+                    <input className="credit-to-add">Enter amount</input>
+                    <button className="add-credit">Add Credit</button>
+                </div>
+                <div className="order-summary-container">
+                    <div className="order-summary-title">Order Summary</div>
+                    <div>menu items</div>
+                    <div className="order-total">Order Total (Subtotal): </div>
+                    <div className="tip-title">Tip</div>
+                    <input className="tip-to-add">Enter tip amount</input>
+                    <button className="add-tip">Add Tip</button>
+                    <div className="tip-amount">Tip Amount: </div>
+                    <div className="grand-total">Grand Total: </div>
+                    <button className="pay-now">Pay Now</button>
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
