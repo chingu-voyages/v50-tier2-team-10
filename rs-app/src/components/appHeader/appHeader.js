e09c2c9c8d037895678b1bfbdd95063ae8b47521
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-13 14:55:30 +1200

    centered app title in header

--- rs-app/src/components/appHeader/appHeader.js
+++ rs-app/src/components/appHeader/appHeader.js
@@ -12,12 +12,15 @@ const AppHeader = () => {
 
 
     return (
-        <header className='header'>
-            <img className='rs-logo' src={rsLogo} alt='cloche with RS written on top'/>
-            <img className='cart-icon' src={cartIcon} alt='shopping cart icon'/>
-            <h1>Restaurant Simulator</h1>
-            <h2>Easy Ordering, Yummy Meals</h2>
-
+        <header className='header-page'>
+            <div className='header-top'>
+                <img className='rs-logo' src={rsLogo} alt='cloche with RS written on top'/>
+                <img className='cart-icon' src={cartIcon} alt='shopping cart icon'/>
+            </div>
+            <div className='header-container'>
+                <h1 className='title'>Restaurant Simulator</h1>
+                <h2 className='sub-title'>Easy Ordering, Yummy Meals</h2>
+            </div>
         </header>
     );
 };
