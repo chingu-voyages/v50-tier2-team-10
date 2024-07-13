26b72f9ab789cf6f6143b7b3317defee901ccddf
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-13 13:51:38 +1200

    added border around menu item card

--- rs-app/src/components/menu/menu.js
+++ rs-app/src/components/menu/menu.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './menu.css';
 
 const Menu = () => {
 
@@ -8,17 +9,18 @@ const Menu = () => {
 
     return (
         <div>
-            Menu items will go here
-            <div className="item-card">
-                <img className="item-image" src=""/>
-                <div className="item-details">
-                    <h3 className="restaurant-name">Restaurant name</h3> {/* insert props  */}
-                    <div className="item-description">Description:</div>
-                    <div className="item-price">Price:</div>
-                    <div className="item-country">Country: </div>
-                </div>
-                <div className="order-button">
-                    <button>Add to Order</button>
+            <div className="menu-items">
+                <div className="item-card">
+                    <img className="item-image" src=""/>
+                    <div className="item-details">
+                        <h3 className="restaurant-name">Restaurant name</h3> {/* insert props  */}
+                        <div className="item-description">Description:</div>
+                        <div className="item-price">Price:</div>
+                        <div className="item-country">Country: </div>
+                    </div>
+                    <div className="order-button">
+                        <button>Add to Order</button>
+                    </div>
                 </div>
             </div>
         </div>
