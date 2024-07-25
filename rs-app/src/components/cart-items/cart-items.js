08d30ccc8461028cc4afe61a3ecc6c51f03a8cc2
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-26 11:56:18 +1200

    added cartItems attribution

--- rs-app/src/components/cart-items/cart-items.js
+++ rs-app/src/components/cart-items/cart-items.js
@@ -4,6 +4,7 @@ const CartItems = ({cart, removeFromCart}) => {
 
     return (
         <>
+        {/* code snippet from https://dev.to/papasanto/build-a-react-hooks-shopping-cart-with-usestate-and-useeffect-39hk on 2024-07-26 */}
         <div className="cart-items-container">
             {cart.map((item) => (
                 <div key={item.id}>
