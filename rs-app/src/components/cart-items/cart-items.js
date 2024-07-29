291832a839844eac84010e56c7b5f6be323d77ea
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-29 15:17:21 +1200

    updated cart items styling

--- rs-app/src/components/cart-items/cart-items.js
+++ rs-app/src/components/cart-items/cart-items.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./cart-items.css";
 
 const CartItems = ({cart, removeFromCart}) => {
 
@@ -7,9 +8,11 @@ const CartItems = ({cart, removeFromCart}) => {
         {/* code snippet from https://dev.to/papasanto/build-a-react-hooks-shopping-cart-with-usestate-and-useeffect-39hk on 2024-07-26 */}
         <div className="cart-items-container">
             {cart.map((item) => (
-                <div key={item.id}>
-                    {`${item.dsc}: $${item.price}`}
-                    <input type="submit" value="remove" onClick={() => removeFromCart(item)} />
+                <div className="ordered-item" key={item.id}>
+                    <div className="restaurant">{item.name}</div>
+                    <button onClick={() => removeFromCart(item)}>Remove</button>
+                    <div className="item-description">{item.dsc}</div>
+                    <div className="item-price">Price: ${item.price}</div>
                 </div>
             ))}
         </div>
