7cbb4934e584a4a7e4f42b63cc2f23ef48375f75
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-08-06 13:03:58 +1200

    added full cart icon when items are added to cart

--- rs-app/src/components/appHeader/appHeader.js
+++ rs-app/src/components/appHeader/appHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import cartIcon from '../../images/cart_icon_orange.png';
 import rsLogo from '../../images/logo_orange.png';
+import cartFull from "../../images/cart_filled_2.png";
 import './appHeader.css';
 
 
-const AppHeader = ({toggleCart}) => {
+const AppHeader = ({toggleCart, cart}) => {
 
 
     return (
@@ -12,7 +13,9 @@ const AppHeader = ({toggleCart}) => {
             <div className='header-top'>
                 <a href="#"><img className='rs-logo' src={rsLogo} alt='cloche with fork and knife on top'/></a>
                 <button className='cart-button' onClick={toggleCart}>
-                    <img className='cart-icon' src={cartIcon} alt='shopping cart icon'/>
+                    {(cart.length) > 0 ?  <img className='cart-icon' src={cartFull} alt='cart with items'/> :
+                    <img className='cart-icon' src={cartIcon} alt='shopping cart icon'/>}
+                    
                 </button>
             </div>
             <div className='header-container'>
